94f0097ccbd8b658d46ef218be9a2b8691489c7e
dtnlong1 <dtnlong04@gmail.com>
2024-04-05 23:00:51 -0500

    add auth

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const compression = require("compression");
 const express = require("express");
 const { default: helmet } = require("helmet");
@@ -8,10 +9,17 @@ const app = express();
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
+app.use(express.json());
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
+
 // init db
-
+require("./dbs/init.mongodb");
 // init routes
-
+app.use("/", require("./routes"));
 // handling error
 
 module.exports = app;
---
+++ src/dbs/init.mongodb.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+class Database {
+	constructor() {
+		this.connect();
+	}
+
+	// connect
+	connect(type = "mongodb") {
+		if (1 === 1) {
+			mongoose.set("debug", true);
+			mongoose.set("debug", { color: true });
+		}
+		mongoose
+			.connect(process.env.MONG_URI, {
+				maxPoolSize: 50,
+			})
+			.then((_) => console.log(`Connected to database`))
+			.catch((err) => console.log(`Error: ${err}`));
+	}
+
+	static getInstance() {
+		if (!Database.instance) {
+			Database.instance = new Database();
+		}
+		return Database.instance;
+	}
+}
+
+const instanceMongodb = Database.getInstance();
+module.exports = instanceMongodb;
