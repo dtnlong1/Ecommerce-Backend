71b43bcba33170157409c51adab262c7e4734f54
dtnlong1 <dtnlong04@gmail.com>
2024-07-18 16:14:18 +0700

    add utils functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -10,7 +10,34 @@ const getSelectData = (select = []) => {
 	return Object.fromEntries(select.map(el => [el, 1]))
 }
 
+const removeUndefinedObject = obj => {
+	Object.keys(obj).forEach(k => {
+		if(obj[k] == null ) {
+			delete obj[k]
+		}
+	})
+	return obj
+}
+
+const updateNestedObjectParser = obj => {
+	const final = {}
+	Object.keys(obj).forEach(k => {
+		if(typeof obj[k] === 'Object' && !Array.isArray(obj[k])) {
+			const response = updateNestedObjectParser(obj[k])
+			Object.keys(response).forEach(a => {
+				final[`${k}.${a}`] = response[a]
+			})
+		}
+		else {
+			final[k] = obj[k]
+		}
+	})
+	return final
+}
+
 module.exports = {
 	getInforData,
-	getSelectData
+	getSelectData, 
+	removeUndefinedObject,
+	updateNestedObjectParser
 };
